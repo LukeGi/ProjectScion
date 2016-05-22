f0ae4d9192f3ba9513f1f8d196397fc1122c8f06
bluemonster122 <boo122333@gmail.com>
2016-05-22 02:26:40 +0100

    He was left behind

--- src/main/java/com/bluemonster122/projectscion/common/items/tools/ItemIronChainsaw.java
+++ src/main/java/com/bluemonster122/projectscion/common/items/tools/ItemIronChainsaw.java
@@ -1,16 +1,19 @@
-package com.bluemonster122.projectscion.common;
+package com.bluemonster122.projectscion.common.items.tools;
 
-import com.bluemonster122.projectscion.common.items.Items;
+import com.bluemonster122.projectscion.ProjectScionCreativeTabs;
+import com.bluemonster122.projectscion.common.items.ItemBase;
+import com.bluemonster122.projectscion.common.items.ModItems;
+import com.bluemonster122.projectscion.common.util.IProvideEvent;
+import com.bluemonster122.projectscion.common.util.InventoryHelper;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.EnumHand;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +22,45 @@ import java.util.Stack;
 /**
  * Created by Blue <boo122333@gmail.com>.
  */
-public class EventHandler {
+public class ItemIronChainsaw extends ItemBase implements IProvideEvent{
+
+    public ItemIronChainsaw() {
+
+        super("ironchainsaw");
+        setCreativeTab(ProjectScionCreativeTabs.tabGeneral);
+        setInternalName("ironchainsaw");
+        setMaxStackSize(1);
+        setMaxDamage(875);
+    }
+
+    @Override
+    public boolean onBlockStartBreak(ItemStack itemstack, BlockPos pos, EntityPlayer player) {
+
+        if (player.worldObj.getBlockState(pos).getBlock().isWood(player.worldObj, pos)) {
+            return true;
+        } else {
+            return super.onBlockStartBreak(itemstack, pos, player);
+        }
+    }
+
+    public float getStrVsBlock(ItemStack stack, IBlockState state) {
+
+        Material material = state.getMaterial();
+        return material != Material.wood && material != Material.leaves ? super.getStrVsBlock(stack, state) : 8.0F;
+    }
+
+    @Override
+    public boolean canHarvestBlock(IBlockState blockIn) {
+
+        Material material = blockIn.getMaterial();
+        return material == Material.leaves || material == Material.wood;
+    }
+
 
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public void startBreak(net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed event) {
 
-        if (event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND).getItem() == Items.IRON_CHAINSAW.getItem()) {
+        if (event.getEntityPlayer().getHeldItemMainhand() != null && event.getEntityPlayer().getHeldItemMainhand().getItem() == ModItems.IRON_CHAINSAW.getItem()) {
             if (event.getEntityPlayer().worldObj.getBlockState(event.getPos()).getBlock().isWood(event.getEntityPlayer().worldObj, event.getPos())) {
                 if (event.getEntityPlayer().isSneaking()) {
                     event.setNewSpeed(event.getOriginalSpeed() * 0.2F);
@@ -39,7 +75,7 @@ public class EventHandler {
         World world = event.getWorld();
         BlockPos pos = event.getPos();
         IBlockState state = event.getState();
-        if (event.getPlayer().getHeldItem(EnumHand.MAIN_HAND).getItem() == Items.IRON_CHAINSAW.getItem()) {
+        if (event.getPlayer().getHeldItemMainhand() != null && event.getPlayer().getHeldItemMainhand().getItem() == ModItems.IRON_CHAINSAW.getItem()) {
             if (state.getBlock().isWood(world, pos)) { // ALL CONDITIONS MET BY HERE
                 List<BlockPos> wood = new ArrayList<>();
                 Stack<BlockPos> toVisit = new Stack<>();
@@ -70,23 +106,20 @@ public class EventHandler {
                 });
                 if (event.getPlayer().isSneaking()) {
                     for (int i = 0; i < 10 && !wood.isEmpty(); i++) {
-                        world.destroyBlock(wood.get(0), true);
-                        event.getPlayer().getHeldItem(EnumHand.MAIN_HAND).damageItem(1, event.getPlayer());
+                        BlockPos currentPos = wood.get(0);
                         wood.remove(0);
+                        InventoryHelper.breakBlockIntoPlayerInv(world, currentPos, event.getPlayer().getHeldItemMainhand(), event.getPlayer());
+                        event.getPlayer().getHeldItemMainhand().damageItem(1, event.getPlayer());
                     }
                     event.setCanceled(true);
                 } else {
-                    world.destroyBlock(wood.get(0), true);
-                    event.getPlayer().getHeldItem(EnumHand.MAIN_HAND).damageItem(1, event.getPlayer());
+                    BlockPos currentPos = wood.get(0);
+                    wood.remove(0);
+                    InventoryHelper.breakBlockIntoPlayerInv(world, currentPos, event.getPlayer().getHeldItemMainhand(), event.getPlayer());
+                    event.getPlayer().getHeldItemMainhand().damageItem(1, event.getPlayer());
                     event.setCanceled(true);
                 }
             }
         }
-    }
-
-    @SubscribeEvent
-    public void login(PlayerEvent.PlayerLoggedInEvent event) {
-
-        event.player.addChatComponentMessage(new TextComponentString(TextFormatting.DARK_AQUA + "Welcome to the world!"));
     }
 }
